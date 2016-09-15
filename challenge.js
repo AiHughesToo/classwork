ffdd5573a276fa1588f000182d75b88162dfc137
Alex Hughes <alexhughes@AlexHughesMBP.local>
2016-09-15 09:54:13 -0400

    khv

---
+++ challenge.js
@@ -0,0 +1,19 @@
+var pWins = 0;
+var cWins = 3;
+
+if (pWins > cWins){
+
+console.log("talk smack.");
+
+}else if (pWins == cWins) {
+
+console.log("its a tie");
+
+} else if(pWins < cWins && pWins !== 0) 
+{
+console.log("You suck!");
+
+} else if (pWins == 0) {
+
+console.log("cry");
+}
---
+++ condition.js
@@ -0,0 +1,13 @@
+var inchesOfSnow = 10;
+
+if (inchesOfSnow >=12) {
+
+console.log("It's a snow day!");
+
+
+}
+else{
+
+console.log("text here.");
+
+}
